f705346410a0e76ae5e3994955c9fb427a745d01
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2020-07-06 20:57:00 +1000

    added second test with more realistic host and viral sequences

--- src/sim_tests/test-simple.sh
+++ src/sim_tests/test-simple.sh
@@ -36,4 +36,30 @@ python3 ${TESTHOSTSCRIPT} \
 --int-info ${LOCS} \
 --host-fa ${HOST} \
 
+printf "\n\n"
 
+HOST="../../data/references/test_human.fa"
+VIRUS="../../data/references/test_AAV.fa"
+
+i=1
+INTS="${OUTDIR}integrations_${i}.fa"
+LOCS="${OUTDIR}ints_info_${i}.txt"
+EPI="${OUTDIR}epi_info_${i}.txt"
+
+python3 ${SCRIPT} \
+ --host ${HOST} \
+ --virus ${VIRUS} \
+ --ints ${INTS} \
+ --int_info ${LOCS} \
+ --int_num 10 \
+ --seed 1 \
+ --epi_num 5 \
+ --epi_info ${EPI} \
+ --p_host_deletion 0.9 \
+ --lambda_host_deletion 2 \
+ --verbose
+ 
+python3 ${TESTHOSTSCRIPT} \
+--int-fa ${INTS} \
+--int-info ${LOCS} \
+--host-fa ${HOST} \

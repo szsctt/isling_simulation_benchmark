e546def8e8870b278252147f5843e79027a6045c
Scott, Suzanne (H&B, North Ryde) <Suzanne.Scott@csiro.au>
2020-07-06 21:35:11 +1000

    added a few more tests

--- src/sim_tests/test-simple.sh
+++ src/sim_tests/test-simple.sh
@@ -34,17 +34,20 @@ TESTHOSTSCRIPT="../../intvi_simulation/reconstruct_host.py"
 python3 ${TESTHOSTSCRIPT} \
 --int-fa ${INTS} \
 --int-info ${LOCS} \
---host-fa ${HOST} \
+--host-fa ${HOST} 
+
+TESTVIRUSCRIPT="../../intvi_simulation/reconstruct_virus.py"
+python3 ${TESTVIRUSCRIPT} \
+--int-fa ${INTS} \
+--int-info ${LOCS} \
+--virus-fa ${VIRUS} 
+
+
 
 printf "\n\n"
 
 HOST="../../data/references/test_human.fa"
 VIRUS="../../data/references/test_AAV.fa"
-
-i=1
-INTS="${OUTDIR}integrations_${i}.fa"
-LOCS="${OUTDIR}ints_info_${i}.txt"
-EPI="${OUTDIR}epi_info_${i}.txt"
 
 python3 ${SCRIPT} \
  --host ${HOST} \
@@ -62,4 +65,70 @@ python3 ${SCRIPT} \
 python3 ${TESTHOSTSCRIPT} \
 --int-fa ${INTS} \
 --int-info ${LOCS} \
---host-fa ${HOST} \
+--host-fa ${HOST} 
+
+python3 ${TESTVIRUSCRIPT} \
+--int-fa ${INTS} \
+--int-info ${LOCS} \
+--virus-fa ${VIRUS} 
+
+# change seed
+printf "\n\n"
+
+python3 ${SCRIPT} \
+ --host ${HOST} \
+ --virus ${VIRUS} \
+ --ints ${INTS} \
+ --int_info ${LOCS} \
+ --int_num 10 \
+ --seed 12345 \
+ --epi_num 5 \
+ --epi_info ${EPI} \
+ --p_host_deletion 0.9 \
+ --lambda_host_deletion 2 \
+ --verbose
+ 
+python3 ${TESTHOSTSCRIPT} \
+--int-fa ${INTS} \
+--int-info ${LOCS} \
+--host-fa ${HOST} 
+
+python3 ${TESTVIRUSCRIPT} \
+--int-fa ${INTS} \
+--int-info ${LOCS} \
+--virus-fa ${VIRUS} 
+
+# check that two runs with the same seed produce the same results
+printf "\n\n"
+
+i=1
+INTS1="${OUTDIR}integrations_${i}.fa"
+LOCS1="${OUTDIR}ints_info_${i}.txt"
+EPI1="${OUTDIR}epi_info_${i}.txt"
+
+python3 ${SCRIPT} \
+ --host ${HOST} \
+ --virus ${VIRUS} \
+ --ints ${INTS1} \
+ --int_info ${LOCS1} \
+ --int_num 10 \
+ --seed 12345 \
+ --epi_num 5 \
+ --epi_info ${EPI1} \
+ --p_host_deletion 0.9 \
+ --lambda_host_deletion 2 \
+ --verbose
+ 
+python3 ${TESTHOSTSCRIPT} \
+--int-fa ${INTS1} \
+--int-info ${LOCS1} \
+--host-fa ${HOST} 
+
+python3 ${TESTVIRUSCRIPT} \
+--int-fa ${INTS1} \
+--int-info ${LOCS1} \
+--virus-fa ${VIRUS} 
+
+diff ${INTS} ${INTS1}
+diff ${LOCS} ${LOCS1}
+diff ${EPI} ${EPI1}

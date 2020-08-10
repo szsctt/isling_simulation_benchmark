6ebceb4b419f3f614a78ac73e2035652c620965e
Suzanne Scott <suzanne.scott@csiro.au>
2020-08-10 22:34:12 +1000

    updated scripts

--- src/sim_tests/test-analysis-pipeline.sh
+++ src/sim_tests/test-analysis-pipeline.sh
@@ -7,4 +7,5 @@ conda activate snakemake
 module load singularity
 
 cd ../../intvi_pipeline 
-snakemake --configfile ../config/test/detection.yml --cores 1 --use-singularity
+#snakemake --configfile ../config/test/detection.yml --cores 1 --use-singularity --dag | dot -Tsvg > ../out/test_dag.svg
+snakemake --configfile ../config/test/detection.yml --jobs 50 --use-singularity  --profile slurm --rerun-incomplete
--- src/sim_tests/test-sim-analysis_combined.sh
+++ src/sim_tests/test-sim-analysis_combined.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 set -e
 
+SIM="../../intvi_simulation"
+ANALYSIS="../../intvi_pipeline"
+
 eval "$(conda shell.bash hook)"
 conda activate snakemake
 
 module load singularity
 
-#cd ../snakemake_sim_analysis 
-#snakemake --configfile ../../config/test/sim_and_detect.yml --cores 1 --use-singularity
+cd ../snakemake_sim_analysis 
 
-cd ../../intvi_simulation 
-echo "running simulation..."
-snakemake --configfile ../config/test/simulation.yml --jobs 50 --use-singularity --profile slurm --rerun-incomplete
+mkdir -p snakemake_rules
+ln -sf $(realpath "$SIM/snakemake_rules/"*smk) $(realpath snakemake_rules)
+ln -sf "$SIM/snakemake_rules/make_df.py" snakemake_rules
+ln -sf "$SIM/snakemake_rules/make_df.py" snakemake_rules
+ln -sf "$SIM/scripts" .
 
-cd ../intvi_pipeline 
-echo ""
-echo "running analysis..."
-snakemake --configfile ../config/test/detection.yml --jobs 50 --use-singularity --profile slurm --rerun-incomplete --cluster-config ../config/test/detection-cluster.json
+ln -sf $(realpath "$ANALYSIS/snakemake_rules/"*smk) $(realpath snakemake_rules)
+ln -sf $ANALYSIS/*.pl .
+ln -sf $ANALYSIS/*.R .
+ln -sf $ANALYSIS/*.sh .
+ln -sf $ANALYSIS/post .
+
+
+# python stuff
+mkdir -p python_scripts
+ln -sf $(realpath "$SIM/snakemake_rules/parse_config.py") $(realpath python_scripts)
+ln -sf $(realpath "$ANALYSIS/snakemake_rules/make_df.py") $(realpath python_scripts)
+
+snakemake --snakefile combined_snakefile --configfile ../../config/test/sim_and_detect.yml --cores 1 --use-singularity --profile slurm
+
+

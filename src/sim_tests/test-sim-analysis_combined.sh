f0381af7fc7e6594cd4626894aff9bfc73a6d6f3
Suzanne Scott <suzanne.scott@csiro.au>
2020-08-11 17:50:34 +1000

    added dag output for troubleshooting

--- src/sim_tests/test-sim-analysis_combined.sh
+++ src/sim_tests/test-sim-analysis_combined.sh
@@ -8,11 +8,15 @@ module load singularity
 
 cd ../snakemake_sim_analysis 
 
+snakemake --configfile ../../config/test/sim_and_detect.yml --snakefile combined_snakefile --dag | dot -Tsvg > ../../out/test_combined.dag.svg
+
 snakemake \
  --snakefile combined_snakefile \
- --configfile ../../config/test/sim_and_detect.yml \
+ --configfile ../../config/test/sim_and_detect.yml\
  --jobs 50 \
  --use-singularity \
  --profile slurm
+ 
+ 
 
 

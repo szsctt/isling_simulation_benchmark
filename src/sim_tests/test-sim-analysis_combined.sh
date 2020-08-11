846915f55f514c9a44082310c0d4b19859cb6be0
Suzanne Scott <suzanne.scott@csiro.au>
2020-08-11 15:45:48 +1000

    moved symlinks to different script

--- src/sim_tests/test-sim-analysis_combined.sh
+++ src/sim_tests/test-sim-analysis_combined.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-SIM="../../intvi_simulation"
-ANALYSIS="../../intvi_pipeline"
-
 eval "$(conda shell.bash hook)"
 conda activate snakemake
 
@@ -11,24 +8,11 @@ module load singularity
 
 cd ../snakemake_sim_analysis 
 
-mkdir -p snakemake_rules
-ln -sf $(realpath "$SIM/snakemake_rules/"*smk) $(realpath snakemake_rules)
-ln -sf "$SIM/snakemake_rules/make_df.py" snakemake_rules
-ln -sf "$SIM/snakemake_rules/make_df.py" snakemake_rules
-ln -sf "$SIM/scripts" .
-
-ln -sf $(realpath "$ANALYSIS/snakemake_rules/"*smk) $(realpath snakemake_rules)
-ln -sf $ANALYSIS/*.pl .
-ln -sf $ANALYSIS/*.R .
-ln -sf $ANALYSIS/*.sh .
-ln -sf $ANALYSIS/post .
-
-
-# python stuff
-mkdir -p python_scripts
-ln -sf $(realpath "$SIM/snakemake_rules/parse_config.py") $(realpath python_scripts)
-ln -sf $(realpath "$ANALYSIS/snakemake_rules/make_df.py") $(realpath python_scripts)
-
-snakemake --snakefile combined_snakefile --configfile ../../config/test/sim_and_detect.yml --cores 1 --use-singularity --profile slurm
+snakemake \
+ --snakefile combined_snakefile \
+ --configfile ../../config/test/sim_and_detect.yml \
+ --jobs 50 \
+ --use-singularity \
+ --profile slurm
 
 

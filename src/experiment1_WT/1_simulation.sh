14b3fa04c454095e9622dc196d0c59b40714de42
Suzanne Scott <suzanne.scott@csiro.au>
2020-08-12 10:16:25 +1000

    removed scripts

--- src/experiment1_WT/1_simulation.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-eval "$(conda shell.bash hook)"
-conda activate snakemake
-module load singularity
-
-cd ../../intvi_simulation 
-snakemake --configfile ../config/experiment1/simulation.yml --jobs 50 --use-singularity --profile slurm

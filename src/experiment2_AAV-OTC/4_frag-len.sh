b98b37aecdbd552653afd2f4a0a7efd167bdaf2d
Suzanne Scott <suzanne.scott@csiro.au>
2020-08-17 20:27:05 +1000

    runscripts for OTC vector experiments

---
+++ src/experiment2_AAV-OTC/4_frag-len.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+CONFIG="../../config/experiment2_AAV-OTC/frag-len.yml"
+CLUSTER="../../config/experiment2_AAV-OTC/cluster.json"
+OUTPATH="../../out/experiment2_AAV-OTC/"
+NAME="chomosomes"
+
+eval "$(conda shell.bash hook)"
+conda activate snakemake
+
+module load singularity
+
+cd ../snakemake_sim_analysis 
+
+mkdir -p ${OUTPATH}
+snakemake --configfile ${CONFIG} --snakefile combined_snakefile --dag | dot -Tsvg > ${OUTPATH}/${NAME}.dag.svg
+
+snakemake \
+ --snakefile combined_snakefile \
+ --configfile ${CONFIG}\
+ --jobs 50 \
+ --use-singularity \
+ --profile slurm \
+ --cluster-config ${CLUSTER}
+ 
+

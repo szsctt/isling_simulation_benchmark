696b90bf247840430edb439ab239a49e85fd0ec3
Suzanne Scott <suzanne.scott@csiro.au>
2020-09-16 11:55:55 +1000

    added second experiment to break down harder condition

---
+++ src/experiment0_prelim/2_condition-breakdown.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+CONFIG="../../config/experiment0_prelim/condition-breakdown.yml"
+CLUSTER="../../config/experiment0_prelim/cluster.json"
+OUTPATH="../../out/experiment0_prelim/"
+NAME="condition-breakdown"
+
+eval "$(conda shell.bash hook)"
+conda activate snakemake
+
+module load singularity
+
+cd ../snakemake_sim_analysis 
+
+mkdir -p ${OUTPATH}
+snakemake --configfile ${CONFIG} --snakefile combined_snakefile --dag | dot -Tsvg > ${OUTPATH}/${NAME}.dag.svg
+
+snakemake \
+ --snakefile combined_snakefile \
+ --configfile ${CONFIG}\
+ --jobs 1 \
+ --use-singularity \
+ --profile slurm \
+ --cluster-config ${CLUSTER}
+ 
+

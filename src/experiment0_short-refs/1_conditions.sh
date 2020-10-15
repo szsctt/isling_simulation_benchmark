95cddb88f36dca57760e392796ca3c007aa831a6
Suzanne Scott <suzanne.scott@csiro.au>
2020-10-15 15:16:59 +1100

    made experiment 0 use short references

---
+++ src/experiment0_short-refs/1_conditions.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+CONFIG="../../config/experiment0_short-refs/virus.yml"
+CLUSTER="../../config/experiment0_short-refs/cluster.json"
+OUTPATH="../../out/experiment0_short-refs/"
+NAME="conditions"
+
+eval "$(conda shell.bash hook)"
+conda activate snakemake
+
+module load singularity
+
+cd ../snakemake_sim_analysis 
+
+mkdir -p ${OUTPATH}
+snakemake --configfile ${CONFIG} --snakefile combined_snakefile --dag | dot -Tsvg > ${OUTPATH}/${NAME}.dag.svg
+
+snakemake \
+ --snakefile combined_snakefile \
+ --configfile ${CONFIG}\
+ --jobs 100 \
+ --use-singularity \
+ --profile slurm \
+ --cluster-config ${CLUSTER}
+ 
+
+

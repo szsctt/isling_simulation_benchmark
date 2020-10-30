6bb9a95d2b80e6cb2e014d2ca41f27e725eae7b0
Suzanne Scott <suzanne.scott@csiro.au>
2020-10-30 20:53:52 +1100

    removed unnecessary ilnes

--- src/experiment1_OTC_chr1/run_exp1.sh
+++ src/experiment1_OTC_chr1/run_exp1.sh
@@ -9,15 +9,11 @@ NAME="easier-harder"
 bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
 CONFIG="../../config/experiment1_OTC_chr1/read-properties.yml"
-CLUSTER="../../config/experiment1_OTC_chr1/cluster.json"
-OUTPATH="../../out/experiment1_OTC_chr1/"
 NAME="read-properties"
 
 bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
 CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown.yml"
-CLUSTER="../../config/experiment1_OTC_chr1/cluster.json"
-OUTPATH="../../out/experiment1_OTC_chr1/"
 NAME="condition-breakdown"
 
 bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME

d6b17e2ad250ac8b6ca91f2c64c7cf62e4c4cd2c
Suzanne Scott <suzanne.scott@csiro.au>
2020-11-13 12:35:21 +1100

    refactor combined_snakefile to make other tools easier to run in other workflows"

--- src/snakemake_sim_analysis/link_scripts.sh
+++ src/snakemake_sim_analysis/link_scripts.sh
@@ -5,27 +5,34 @@ set -e
 
 SIM=../../intvi_simulation
 ANALYSIS=../../intvi_pipeline
+TOOLS=../../intvi_other-tools
 
 # simulation stuff
 mkdir -p snakemake_rules
 mkdir -p scripts
-find $SIM/snakemake_rules -name "*smk" -exec ln -sf  $(realpath {}) $(realpath snakemake_rules) \;
-find $SIM/scripts -name "*" -exec ln -sf $(realpath {}) $(realpath scripts) \;
+find $SIM/snakemake_rules -name "*smk" -exec ln -sf  $(realpath {}) $(realpath snakemake_rules/) \;
+find $SIM/scripts -name "*" -exec ln -sf $(realpath {}) $(realpath scripts/) \;
+
 
 # analysis stuff
-find $ANALYSIS/snakemake_rules -name "*smk" -exec ln -sf $(realpath {}) $(realpath snakemake_rules) \;
-find $ANALYSIS/scripts -name "*.pl" -exec ln -sf $(realpath {}) $(realpath scripts) \;
-find $ANALYSIS/scripts -name "*.pm" -exec ln -sf $(realpath {}) $(realpath scripts) \;
-find $ANALYSIS/scripts -name "*.R" -exec ln -sf $(realpath {}) $(realpath scripts) \;
-find $ANALYSIS/scripts -name "*.sh" -exec ln -sf $(realpath {}) $(realpath scripts) \;
-find $ANALYSIS/scripts -name "post" -exec ln -sf $(realpath {}) $(realpath scripts) \;
+find $ANALYSIS/snakemake_rules -name "*smk" -exec ln -sf $(realpath {}) $(realpath snakemake_rules/) \;
+find $ANALYSIS/scripts -name "*.pl" -exec ln -sf $(realpath {}) $(realpath scripts/) \;
+find $ANALYSIS/scripts -name "*.pm" -exec ln -sf $(realpath {}) $(realpath scripts/) \;
+find $ANALYSIS/scripts -name "*.R" -exec ln -sf $(realpath {}) $(realpath scripts/) \;
+find $ANALYSIS/scripts -name "*.sh" -exec ln -sf $(realpath {}) $(realpath scripts/) \;
+find $ANALYSIS/scripts -name "post" -exec ln -sf $(realpath {}) $(realpath scripts/) \;
+
+# other tools stuff
+find $TOOLS/scripts -name "*py" -exec ln -sf $(realpath {}) $(realpath scripts/) \;
+find $TOOLS/snakemake_rules -name "*smk" -exec ln -sf $(realpath {}) $(realpath snakemake_rules/) \;
 
 # python stuff
 mkdir -p python_scripts
-find $SIM/snakemake_rules -name "parse_config.py" -exec ln -sf $(realpath {}) $(realpath python_scripts) \;
-find $ANALYSIS/snakemake_rules -name "make_df.py" -exec ln -sf $(realpath {}) $(realpath python_scripts) \;
+find $SIM/snakemake_rules -name "parse_config.py" -exec ln -sf $(realpath {}) $(realpath python_scripts/) \;
+find $ANALYSIS/snakemake_rules -name "make_df.py" -exec ln -sf $(realpath {}) $(realpath python_scripts/) \;
+
 
 # conda envs
 mkdir -p envs
-find $SIM/envs -name "*.yml" -exec ln -sf $(realpath {}) $(realpath envs) \;
-find $ANALYSIS/envs -name "*.yml" -exec ln -sf $(realpath {}) $(realpath envs) \;
+find $SIM/envs -name "*.yml" -exec ln -sf $(realpath {}) $(realpath envs/) \;
+find $ANALYSIS/envs -name "*.yml" -exec ln -sf $(realpath {}) $(realpath envs/) \;

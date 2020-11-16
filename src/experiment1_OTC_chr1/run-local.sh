d65abe51f9968f4d0bd381dd85efc54518278198
Suzanne Scott <suzanne.scott@csiro.au>
2020-11-16 23:25:19 +1100

    updated local run

--- src/experiment1_OTC_chr1/run-local.sh
+++ src/experiment1_OTC_chr1/run-local.sh
@@ -6,27 +6,17 @@ CONFIG=$1
 
 cd ../snakemake_sim_analysis 
 
-# need to strip "../.." from input and output file paths in config file
-filename="$(dirname $CONFIG)/$(basename $CONFIG .yml)"
-NEW="${filename}.cloud.yml"
-sed 's%../../data%data%g' $CONFIG > $NEW
-sed -i 's%../../out%out%g' $NEW
-
 snakemake \
  --snakefile combined_snakefile \
  --configfile ${CONFIG} \
- --cores 8 \
- --jobs 3 \
+ --cores 1 \
+ --jobs 1 \
  --restart-times 3 \
  --keep-going \
  --resources mem_mb=28000 \
  --use-singularity \
  --singularity-args '-B $(realpath ../..)' \
- --rerun-incomplete \
- --until vifi
+ --rerun-incomplete 
 
-# --configfile ${NEW}\
-# --default-remote-provider "GS" \
-# --default-remote-prefix sjs_snakemake_test
 
 

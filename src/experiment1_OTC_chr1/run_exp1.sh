29bb72c265a6fe13f563ddaa8f3e8a2cb4ce439e
Suzanne Scott <suzanne.scott@csiro.au>
2020-11-24 22:57:48 +1100

    rearranged order

--- src/experiment1_OTC_chr1/run_exp1.sh
+++ src/experiment1_OTC_chr1/run_exp1.sh
@@ -10,24 +10,24 @@ NAME="easier-harder"
 
 bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
-CONFIG="../../config/experiment1_OTC_chr1/analysis-conditions.yml"
-NAME="analysis-conditions"
+CONFIG="../../config/experiment1_OTC_chr1/chromosomes.yml"
+NAME="chromosomes"
 
-#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
-
-CONFIG="../../config/experiment1_OTC_chr1/read-properties.yml"
-NAME="read-properties"
-
-#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
+bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
 CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown.yml"
 NAME="condition-breakdown"
 
-#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
+bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
-CONFIG="../../config/experiment1_OTC_chr1/chromosomes.yml"
-NAME="chromosomes"
+CONFIG="../../config/experiment1_OTC_chr1/analysis-conditions.yml"
+NAME="analysis-conditions"
 
-#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
+bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
+
+CONFIG="../../config/experiment1_OTC_chr1/read-properties.yml"
+NAME="read-properties"
+
+bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
 

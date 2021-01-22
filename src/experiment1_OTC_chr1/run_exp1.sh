83280b46d348c3e30e7a323bea77ac3f3f46cea3
Suzanne Scott <suzanne.scott@csiro.au>
2021-01-22 12:20:14 +1100

    updated config files

--- src/experiment1_OTC_chr1/run_exp1.sh
+++ src/experiment1_OTC_chr1/run_exp1.sh
@@ -8,36 +8,61 @@ OUTPATH="../../out/experiment1_OTC_chr1/"
 CONFIG="../../config/experiment1_OTC_chr1/easier-harder.yml"
 NAME="easier-harder"
 
+echo "running with config $CONFIG"
 bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
-CONFIG="../../config/experiment1_OTC_chr1/chromosomes.yml"
-NAME="chromosomes"
+CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown_OTC-harder-3.yml"
+NAME="condition-breakdown_OTC-harder"
 
+echo "running with config $CONFIG"
+bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
+
+CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown_OTC-harder-2.yml"
+NAME="condition-breakdown_OTC-harder"
+
+#echo "running with config $CONFIG"
 #bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
+
 
 CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown_OTC-harder.yml"
 NAME="condition-breakdown_OTC-harder"
 
-bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
+#echo "running with config $CONFIG"
+#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
+
+
+CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown_2_mapq-15.yml"
+NAME="condition-breakdown-2"
+
+#echo "running with config $CONFIG"
+#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME 
+
+CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown_1_mapq-15.yml"
+NAME="condition-breakdown-1"
+
+#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME 
+
+
+
 
 CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown_2.yml"
 NAME="condition-breakdown-2"
 
-bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME &
-PID_2=$!
+#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME &
+#PID_2=$!
 
 CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown_1.yml"
 NAME="condition-breakdown-1"
 
-bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME &
-PID_1=$!
+#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME &
+#PID_1=$!
 
-wait $PID_1 $PID_2
+#wait $PID_1 $PID_2
 
 CONFIG="../../config/experiment1_OTC_chr1/condition-breakdown_OTC-harder.yml"
 NAME="condition-breakdown_OTC-harder"
 
-bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
+#bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
 CONFIG="../../config/experiment1_OTC_chr1/analysis-conditions.yml"
 NAME="analysis-conditions"

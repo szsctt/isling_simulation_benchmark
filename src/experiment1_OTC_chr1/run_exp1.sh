3b8691840e1792b9c9bde371157796e55c8437ee
Suzanne Scott <suzanne.scott@csiro.au>
2021-02-12 17:06:30 +1100

    updated experient 1 runscript

--- src/experiment1_OTC_chr1/run_exp1.sh
+++ src/experiment1_OTC_chr1/run_exp1.sh
@@ -6,19 +6,19 @@ CLUSTER="../../config/experiment1_OTC_chr1/cluster.json"
 OUTPATH="../../out/experiment1_OTC_chr1/"
 
 CONFIG="../../config/experiment1_OTC_chr1/AAV-OTC_parameter-optimise.yml"
-NAME="easier-harder"
+NAME="AAV-OTC_parameters"
 
 echo "running with config $CONFIG"
 bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
 CONFIG="../../config/experiment1_OTC_chr1/AAV-OTC.yml"
-NAME="easier-harder"
+NAME="AAV-OTC"
 
 echo "running with config $CONFIG"
 bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME
 
 CONFIG="../../config/experiment1_OTC_chr1/OTC-condition-breakdown.yml"
-NAME="condition-breakdown_OTC-harder"
+NAME="condition-breakdown_OTC"
 
 echo "running with config $CONFIG"
 bash ./run.sh $CONFIG $CLUSTER $OUTPATH $NAME

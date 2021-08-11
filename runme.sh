8094249f81c82e44b945f5bcee3246d8ecbc27eb
Suzanne Scott <suzanne.scott@csiro.au>
2021-08-11 16:00:32 +1000

    updated runscript

--- runme.sh
+++ runme.sh
@@ -5,7 +5,18 @@ set -euo pipefail
 # dependencies for running
 conda list -n sim_isling || conda create -n sim_isling -c bioconda -c conda-forge entrez-direct=13.9 snakemake=6.6
 
+
+
+
 # download references
 bash src/references/make_refs.sh
 
+# link necessary scripts in combined snakefile directory
+cd src/snakemake_sim_analysis
+bash link_scripts.sh
+cd ../..
 
+# simulate data and analyse for AAV and OTC
+bash src/experiment1_OTC_chr1/run_exp1_local.sh
+
+# generate figures and tables

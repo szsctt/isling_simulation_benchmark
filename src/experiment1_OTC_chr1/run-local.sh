420a8c48f8af12bea49bf8ecf6b8ceef49bcf7f6
Suzanne Scott <suzanne.scott@csiro.au>
2021-08-12 12:01:39 +1000

    added option to choose number of cores

--- src/experiment1_OTC_chr1/run-local.sh
+++ src/experiment1_OTC_chr1/run-local.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# ./run-cloud.sh <config yml>
+# ./run-cloud.sh <config yml> <cores>
 
 CONFIG=$1
+CORES=$2
 
-cd ../snakemake_sim_analysis 
+cd src/snakemake_sim_analysis
 
 snakemake \
  --snakefile combined_snakefile \
  --configfile ${CONFIG} \
- --cores 1 \
+ --cores ${CORES} \
  --jobs 1 \
  --restart-times 3 \
  --keep-going \
  --resources mem_mb=20000 \
  --use-singularity \
- --singularity-args '-B $(realpath ../..)' \
- --rerun-incomplete 
+ --singularity-args '-B $(realpath ../../..)' \
+ --rerun-incomplete
 
 
 
--- src/experiment1_OTC_chr1/run_exp1_local.sh
+++ src/experiment1_OTC_chr1/run_exp1_local.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+CORES=$1
+
 # run AAV and OTC experiment
 CONFIG="../../config/experiment1_OTC_chr1/AAV-OTC.yml"
-bash src/experiment1_OTC_chr1/run-local.sh $CONFIG
+bash src/experiment1_OTC_chr1/run-local.sh $CONFIG $CORES
 
 # run OTC experiment
 CONFIG="../../config/experiment1_OTC_chr1/OTC-condition-breakdown.yml"
-bash src/experiment1_OTC_chr1/run-local.sh $CONFIG
+bash src/experiment1_OTC_chr1/run-local.sh $CONFIG $CORES
 
 

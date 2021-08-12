12fa1ae86f5d05297ca7cf5deeaec9db6ccc456c
Suzanne Scott <suzanne.scott@csiro.au>
2021-08-12 12:01:16 +1000

    updated runscript

--- runme.sh
+++ runme.sh
@@ -2,11 +2,11 @@
 
 set -euo pipefail
 
+# set number of cores to use
+CORES="1"
+
 # dependencies for running
 conda list -n sim_isling || conda create -n sim_isling -c bioconda -c conda-forge entrez-direct=13.9 snakemake=6.6
-
-
-
 
 # download references
 bash src/references/make_refs.sh
@@ -17,6 +17,11 @@ bash link_scripts.sh
 cd ../..
 
 # simulate data and analyse for AAV and OTC
-bash src/experiment1_OTC_chr1/run_exp1_local.sh
+bash src/experiment1_OTC_chr1/run_exp1_local.sh $CORES
 
 # generate figures and tables
+bash src/experiment1_OTC_chr1/make_figures.sh
+
+# do runtime stuff
+cd src/experiment2_time
+./run_time_local.sh $CORES

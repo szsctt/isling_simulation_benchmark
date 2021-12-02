c4c501c6884e8e3d0dd6262332c9be5bdb2f3a9b
Suzanne Scott <suzanne.scott@csiro.au>
2021-12-02 13:01:36 +1100

    updated script to render rmarkdown results and make figures with new container version and new rmarkdown notebook

--- src/experiment1_OTC_chr1/make_figures.sh
+++ src/experiment1_OTC_chr1/make_figures.sh
@@ -3,12 +3,12 @@
 set -e
 
 CONTAINER="rscripts"
-VERSION="5"
+VERSION="6"
 
 if [ ! -e ${CONTAINER}_${VERSION}.sif ] ; then
 	singularity pull docker://szsctt/${CONTAINER}:${VERSION}
 fi
 
 # run R notebook
-singularity exec ${CONTAINER}_${VERSION}.sif Rscript -e 'rmarkdown::render("analysis/2021-06-15_new-isling-results.Rmd")'
+singularity exec ${CONTAINER}_${VERSION}.sif Rscript -e 'rmarkdown::render("analysis/2021-11-29_simulation-figures.Rmd")'
 
